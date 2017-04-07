962da6c15e87d4b030d810ed71d9806c3375969b
Jordan Beth <jordanbeth14@gmail.com>
2017-04-07 14:12:47 -0400

    added notes about static main

--- Classes/StaticMemberBaby.java
+++ Classes/StaticMemberBaby.java
@@ -6,11 +6,20 @@
  - Refers to the field / method itsself
     - Not unique for each instance
     - Defined for the class declaration
+
+
+ - Non-static methods can reference static methods, but not the other way around
+
+ - Q: Why is main static?
+ - A: To eliminate ambiguity of which constructor should be called, it can be invoked by the runtime engine without having to instantiate an instance of the parent class. Also, it is convention.
 */
 
 public class StaticMemberBaby {
   static int numBabiesMade = 0;
 
+  static void cry(){
+    System.out.println("**cries**");
+  }
 
   public static void main(String[] args){
     StaticMemberBaby.numBabiesMade = 100;
@@ -22,6 +31,8 @@ public class StaticMemberBaby {
     // b1.numBabiesMade equals 2
     // b2.numBabiesMade equals 2
     System.out.println("b1.numBabiesMade = " + b1.numBabiesMade + "\n" + "b2.numBabiesMade = " + b2.numBabiesMade);
+
+    b1.cry();
 
   }
 
